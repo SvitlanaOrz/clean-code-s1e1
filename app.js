f22af7d85924232baf6741d6d60b9f60727355c7
SvitlanaOrz <svitlana.orz@gmail.com>
2023-04-20 10:52:07 +0300

    refactor: change id-name in HTML and JS according to rule 3.2 html-and-css.md

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput = document.getElementById("new-task"); //Add a new task.
+var taskInput = document.getElementById("new-task-item"); //Add a new task.
 var addButton = document.getElementsByTagName("button")[0]; //first button
 var incompleteTaskHolder = document.getElementById("todo-section__items-list"); //ul of #todo-section__items-list
 var completedTasksHolder = document.getElementById(
@@ -57,7 +57,7 @@ var createNewTaskElement = function (taskString) {
 
 var addTask = function () {
   console.log("Add Task...");
-  //Create a new list item with the text from the #new-task:
+  //Create a new list item with the text from the #new-task-item:
   if (!taskInput.value) return;
   var listItem = createNewTaskElement(taskInput.value);
 
